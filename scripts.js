c3e89be025fbbd463a20f1b0737b15deb5a318ab
Marco Antônio <ma.souza.junior@gmail.com>
2018-09-09 11:34:34 -0300

    ENH : Add github icon

--- scripts.js
+++ scripts.js
@@ -1,16 +1,22 @@
 const addCardProject = ({title, desc, link, img}) => {
     const container = $('#projects .row')
+    const demo_link = `https://code-weekend.github.io/${title}/`
 
     if (title === 'code-weekend.github.io') return
 
     // Create Element
     const elementString = `
-    <div class="col s4">
+    <div class="col l4 m6 s12">
         <div class="card">
             <div class="card-image">
                 <img src="${img}">
+                <a target="_blank" href="${demo_link}">
+                    <div style="position: absolute; background: rgba(110,110,110,0.2); width: 100%; height: 100%; top: 0"></div>
+                </a>
                 <span class="card-title">${title}</span>
-                <a target="_blank" href="${link}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                <a target="_blank" href="${link}" class="btn-floating btn-small halfway-fab waves-effect waves-light white" style="padding: 0px">
+                    <img src="./images/mark-github.svg" alt="github"/>
+                </a>
             </div>
 
             <div class="card-content">
